8ac3576ffbf3068d9901396609fc39fca6201099
Alex Aur <alexander.aur@gmail.com>
2012-07-06 00:52:25 -0500

    Added error messages to registration page. Modified javascript file to display the error messages in the registration callback. Modified the registration.php file to check for registration errors: password mismatch, invalid email, etc...

--- assets/php/registration.php
+++ assets/php/registration.php
@@ -11,14 +11,17 @@ if(isset($_POST['newemail']))
 	$email = $_POST['newemail'];
 else
 	$email = "";
+
 if(isset($_POST['newusername']))
 	$username = $_POST['newusername'];
 else
 	$username = "";
+
 if(isset($_POST['newpass']))
 	$password = $_POST['newpass'];
 else
 	$password = "";
+
 if(isset($_POST['newpassconfirm']))
 	$passwordConfirm = $_POST['newpassconfirm'];
 else
@@ -35,7 +38,40 @@ $salt = createSalt();
 $hash = hash('sha256', $salt . $hash);
 
 //sanitize username
-$username = mysql_real_escape_string($username);
+//$username = mysql_real_escape_string($username);
+
+// If any of the below errors occurs, set dieFlag to 1 and die()
+$dieFlag = 0;
+$errArr = Array();
+// Analyze the fields to make sure the data is valid. Otherwise, send back the error.
+// Empty username
+if($username == ""){
+	$errArr['username_analysis'] = "Empty username";
+	$dieFlag = 1;
+}
+// Check if the email is valid.
+$emailPattern = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
+if(!preg_match($emailPattern, $email)){
+	$errArr['email_analysis'] = "Invalid email";
+	$dieFlag = 1;
+}
+
+// Check if password exists
+if($password == "" || $passwordConfirm == ""){
+	$errArr['password_analysis'] = "Empty password";
+	$dieFlag = 1;
+}
+
+if(($password != $passwordConfirm) && ($password != "") && ($passwordConfirm != "")){
+	$errArr['confirm_analysis'] = "Password mismatch";
+	$dieFlag = 1;
+}
+
+// Check if need to die
+if($dieFlag == 1){
+	echo json_encode($errArr);
+	die();
+}
 
 // Connect to the database
 $conn = mysql_connect("$host", "$dbusername", "$sqlpass")or die("cannot connect"); 
@@ -48,18 +84,18 @@ $rows = mysql_num_rows($result);
 if($rows > 0)
 {
 	echo json_encode(array("returnvalue"=>"Username already exists."));
+	
 }
 else //it's ok to add it because it's unique
 {
+
+	// Insert it into the database.
 	$query = "INSERT INTO $tbl_name ( email, username, password, salt )
-			VALUES ( '$email', '$username' , '$hash' , '$salt' );";
+		VALUES ( '$email', '$username' , '$hash' , '$salt' );";
 	mysql_query($query) or die ("error here");
-	
-	
 	echo json_encode(array("returnvalue"=>""));
 
 }
-
 
 //header('Location: login.html');
 
--- javascript/registration.js
+++ javascript/registration.js
@@ -19,15 +19,46 @@ function register(email, uname, pass1, pass2){
 //Function to set the error based on php result
  function registerCallback(res){
  	var response = res.returnvalue;
-	alert(response);
-// 	if(response == "")
-// 	{
-// 		disablePopup();
-// 		//Empty out the registration fields
-// 		$("#register-name").val("");
-// 		$("#register-pass").val("");
-// 		$("#register-passconfirm").val("");
-// 	}
+ 	var username_resp = res.username_analysis;
+ 	var email_resp = res.email_analysis;
+ 	var password_resp = res.password_analysis;
+ 	var confirm_resp = res.confirm_analysis;
+ 	if(username_resp == "Empty username"){
+		$("#register_username_control").addClass("error");
+		$("#register_username_help").html("Please enter a username");	
+ 	}
+ 	else{
+ 		$("#register_username_control").removeClass("error");
+		$("#register_username_help").html("");	
+ 	}
+
+ 	if(email_resp == "Invalid email"){
+ 		$("#register_email_control").addClass("error");
+		$("#register_email_help").html("Please enter a valid email address");	
+ 	}
+ 	else{
+ 		$("#register_email_control").removeClass("error");
+		$("#register_email_help").html("");	
+ 	}
+
+ 	if(password_resp == "Empty password"){
+ 		$("#register_password_control").addClass("error");
+		$("#register_password_help").html("Password empty");
+		$("#register_passwordconfirm_control").addClass("error");
+		$("#register_passwordconfirm_help").html("Password confirmation empty");
+ 	}
+ 	else if(confirm_resp == "Password mismatch"){
+ 		$("#register_password_control").addClass("error");
+		$("#register_passwordconfirm_control").addClass("error");
+		$("#register_password_help").html("Passwords do not match");
+		$("#register_passwordconfirm_help").html("Passwords do not match");
+ 	}
+ 	else{
+ 		$("#register_password_control").removeClass("error");
+		$("#register_password_help").html("");
+		$("#register_passwordconfirm_control").removeClass("error");
+		$("#register_passwordconfirm_help").html("");
+ 	}
 }
 
 $(document).ready(function () {
@@ -35,28 +66,16 @@ $(document).ready(function () {
 	//Register submit button click function
 	//Submitting the registration data using ajax
 	$("#register-button").click(function() {
-		alert("clicked the register button");
 		var email = $("#register_email").val();
 		var username = $("#register_username").val();
 		var password = $("#register_password").val();
 		var passwordConfirm = $("#register_passwordConfirm").val();
 		var errMsg = ""; //error feedback to user
-		
-		//Check for empty fields and basic errors
-		if(!username){
-			errMsg += "\nUsername empty.\n";
-		}
-		if(!password || !passwordConfirm){
-			errMsg += "Password empty.";
-		}
-		if(password != passwordConfirm){
-			errMsg += "Password mismatch.";
-		}
-		//$("#register-error").html(errMsg);
+	
 		
 		//Proceed only if there are no errors
 		if(errMsg == ""){
-			alert("no errors");
+			//alert("no errors");
 			register(email, username, password, passwordConfirm);
 		}
 	});
